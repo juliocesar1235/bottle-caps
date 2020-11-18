0837aac5a5aa5fe41e00a1ff44c1003146316793
Julio Gutierrez <a01282575@itesm.mx>
2020-11-18 01:43:23 -0600

    Image card component with variable src and sizes

---
+++ app/src/components/ImageCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :class="[width, height]" class="mb-8 mr-6 rounded overflow-hidden shadow-lg">
+    <img class="w-full" :src="url" alt="Vue logo">
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ImageCard",
+  props: {
+    url: String,
+    width: {
+      type: String,
+      required: false,
+      default: "w-40"
+    },
+    height: {
+      type: String,
+      required: false,
+      default: "h-41"
+    }
+  }
+}
+</script>
