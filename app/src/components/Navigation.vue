3ff35ef0ae3216dd8fdd026cc16c33095ab3e95e
Sergio Diosdado <sgdc_98@hotmail.com>
2020-11-23 08:34:40 -0600

    Add base components. Add login vue. Protect routes

---
+++ app/src/components/Navigation.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="flex items-center justify-between px-4 py-2 bg-gray-800">
+    <p class="text-3xl text-gray-100">Bottle Caps</p>
+    <div v-if="logged" class="text-gray-100">
+      <p>Logout</p>
+    </div>
+  </nav>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      logged: false,
+    }
+  },
+  mounted() {
+    this.logged = this.$store.getters.getLogged
+  }
+};
+</script>
--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,8 +1,26 @@
 import { createStore } from "vuex";
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    user: {
+      username: "Sergio"
+    },
+    logged: false
+  },
+  mutations: { // sync
+    setUser(state, payload) {
+      state.user = payload
+    },
+    setLogged(state, payload) {
+      state.logged = payload
+    }
+  },
+  actions: { // async
+
+  },
+  modules: {},
+  getters: {
+    getUser: state => state.user,
+    getLogged: state => state.logged
+  }
 });
